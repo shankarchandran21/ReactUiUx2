55e988ac6682d88ce575b540ad03ea65eaff8e89
Shankar Chandran <MT066@MWTGROUP.local>
2023-06-08 17:35:47 +0530

    completed

--- uiux/src/components/Footer/Newsletter.jsx
+++ uiux/src/components/Footer/Newsletter.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-
 import './Newsletter.css';
-
+import SubHeading from '../SubHeading/SubHeading'
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className='app__newsletter'>
+      <div className='app__newsletter-heading'>
+         <SubHeading title='Newsletter'/>
+         <h1 className='headtext__cormorant'>Subscribe to Our Newsletter</h1>
+         <p className='p__opensans'>And never miss latest Updates!</p>
+      </div>
+      <div className='app__newsletter-input flex__center'>
+          <input type="email" placeholder='Enter Your Email Address' />
+          <button type='button' className='custom__button'>Subscribe</button>
+      </div>
+      
   </div>
 );
 
--- uiux/src/container/Gallery/Gallery.jsx
+++ uiux/src/container/Gallery/Gallery.jsx
@@ -12,9 +12,9 @@ const Gallery = () =>{
   const scroll = (direction)=>{
     const {current} = scrollRef;
     if(direction === 'left'){
-      current.scroll -=300
+      current.scrollLeft -=300
     }else{
-      current.scroll +=300
+      current.scrollLeft +=300
     }
   }
 
